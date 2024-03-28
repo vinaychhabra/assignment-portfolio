d4e19a6f36a93337929e1fe3719c6fb854386aa7
vinaychhabra <vchhabra10feb@gmail.com>
2024-03-28 05:52:19 -0400

    mobile responsiveness done, folder restructre, Assign first completed

---
+++ Foundation/foundation.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleMenu = document.getElementById('toggle-menu');
+    var headerCategories = document.querySelector('.header-categories');
+
+    var categories = document.querySelectorAll('.header-categories-list li a');
+    categories.forEach(function(category) {
+        category.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default behavior (i.e., jump to anchor)
+            var targetId = category.getAttribute('href');
+            var targetElement = document.querySelector(targetId);
+            toggleMenu.checked = false;
+            headerCategories.style.display = 'none';
+            smoothScrollTo(targetElement.offsetTop);
+        });
+    });
+
+
+    toggleMenu.addEventListener('change', function() {
+        if (toggleMenu.checked) {
+            headerCategories.style.display = 'block';
+        } else {
+            headerCategories.style.display = 'none';
+        }
+    });
+
+
+    function smoothScrollTo(targetPosition) {
+        var currentPosition = window.pageYOffset;
+        var distance = targetPosition - currentPosition;
+        var startTime = null;
+        var duration = 800; 
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            var timeElapsed = currentTime - startTime;
+            var ease = easeInOut(timeElapsed, currentPosition, distance, duration);
+            window.scrollTo(0, ease);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function easeInOut(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+});
